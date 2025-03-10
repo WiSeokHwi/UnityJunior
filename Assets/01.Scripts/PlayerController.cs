e0eee58e0d5c964f3e359a765374087bdc8dfd5b
WiSeokHwi <tjrgnl2115@gmail.com>
2025-03-10 13:43:26 +0900

    20250310LastFinal

--- Assets/01.Scripts/PlayerController.cs
+++ Assets/01.Scripts/PlayerController.cs
@@ -3,9 +3,10 @@ using UnityEngine;
 public class PlayerController: MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public float speed = 5.0f;
-    public float trunSpeed;
-    public float horizontalInput;
+    private float speed = 20.0f;
+    private float trunSpeed=45.0f;
+    private float horizontalInput;
+    private float forwardInput;
 
     void Start()
     {
@@ -16,9 +17,10 @@ public class PlayerController: MonoBehaviour
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
         //차량을 앞으로 이동합니다.
-        transform.Translate(Vector3.forward*Time.deltaTime*speed);
-        transform.Translate(Vector3.right * Time.deltaTime * trunSpeed * horizontalInput);
+        transform.Translate(Vector3.forward*Time.deltaTime*speed*forwardInput);
+        transform.Rotate(Vector3.up,trunSpeed * Time.deltaTime * horizontalInput);
 
     }
 }
